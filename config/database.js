c7cacb57fe63a696c4af2cc4ec63e3e2f1343b76
drive-infected <yanoosick@gmail.com>
2026-06-08 11:59:35 +0300

    Update content types

--- config/database.js
+++ config/database.js
@@ -2,10 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'bmw_database'),
+      user: env('DATABASE_USERNAME', 'janpodvoyski'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: false,
     },
   },
 });
