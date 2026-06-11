bfe0bb2a6339def75de7796b800f37b694c32b99
drive-infected <yanoosick@gmail.com>
2026-06-11 17:02:22 +0300

    Fix database config to use DATABASE_URL

--- config/database.js
+++ config/database.js
@@ -2,12 +2,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'bmw_database'),
-      user: env('DATABASE_USERNAME', 'janpodvoyski'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: false,
+      connectionString: env('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 });
